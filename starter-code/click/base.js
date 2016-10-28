d0ae5c6e3287438b324e9b1d5e0ccbe9450b2e02
virginiachae <chae.virginia@gmail.com>
2016-10-28 16:27:36 -0700

    finished click challenge

--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -1,5 +1,13 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+  $('span').on('click', display)
+  function display() {
+    //console.log($(this).text());
+    var $spanText = $(this).text()
 
+    var $newLi = $('<li></li>');
+    $('ul').append($newLi);
+    $newLi.text($spanText);
+  };
 })
