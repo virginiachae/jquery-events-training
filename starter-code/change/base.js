5cd7da311e450a9db255c7839a5fe1d2221ed6e4
virginiachae <chae.virginia@gmail.com>
2016-10-27 22:42:31 -0700

    finished change challenge

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -3,5 +3,12 @@ console.log("Sanity Check: JS is working!");
 $(document).ready(function(){
 
   $("#time").text( Date.now() );
+  
+  $('.add').on('change', function(){
+        var left = parseInt($('.add')[0].value);
+        var right = parseInt($('.add')[1].value);
+        var total = (left + right).toString()
+        $('#total').val($('#total').val() + total);
+  });
 
 })
